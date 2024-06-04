329f81ee1347baad93eef6e286fe5885d7c97e95
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-06-04 11:04:34 +0700

    Menambahkan input pesan array integer 3 eleme

--- Exeptionstandart/Exeptionstandart.cpp
+++ Exeptionstandart/Exeptionstandart.cpp
@@ -5,3 +5,14 @@
 //untuk obyek array yang akan kita gunakan
 using namespace std;
 
+int main() {
+
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 8, 4, 2 };
+        //pesan array integer 3 elemen
+        cout << data.at(5) << endl;
+
+    }
+    
+}
